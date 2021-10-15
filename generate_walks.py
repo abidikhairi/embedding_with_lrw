1b0d64a4f87e13ab603145383f07f93e0e792c07
flursky <kh41r1.abidi@gmail.com>
2021-10-15 22:16:49 +0100

    add: utils.py

--- generate_walks.py
+++ generate_walks.py
@@ -1,34 +1,11 @@
 import dgl
 import numpy as np
-from dgl.data import CoraGraphDataset, CiteseerGraphDataset, FraudYelpDataset
 from scipy.spatial.distance import hamming, cosine, euclidean
 
 from walker import LazyRandomWalk, RandomWalk
+from utils import load_citeseer, load_cora, load_yelp
 
 
-def load_cora():
-    dataset = CoraGraphDataset(verbose=False)
-
-    features = dataset[0].ndata['feat']
-    graph = dataset[0].to_networkx().to_undirected()
-
-    return graph, features
-
-def load_citeseer():
-    dataset = CiteseerGraphDataset(verbose=False)
-
-    features = dataset[0].ndata['feat']
-    graph = dataset[0].to_networkx().to_undirected()
-
-    return graph, features
-
-def load_yelp():
-    dataset = FraudYelpDataset()
-    
-    graph = dgl.to_homogeneous(dataset[0]).to_networkx().to_undirected()
-    features = dataset[0].ndata['feature'].numpy()
-
-    return graph, features
 
 def main():
     graph, features = load_yelp()
---
+++ utils.py
@@ -0,0 +1,25 @@
+from dgl.data import CoraGraphDataset, CiteseerGraphDataset, FraudYelpDataset
+
+def load_cora():
+    dataset = CoraGraphDataset(verbose=False)
+
+    features = dataset[0].ndata['feat']
+    graph = dataset[0].to_networkx().to_undirected()
+
+    return graph, features
+
+def load_citeseer():
+    dataset = CiteseerGraphDataset(verbose=False)
+
+    features = dataset[0].ndata['feat']
+    graph = dataset[0].to_networkx().to_undirected()
+
+    return graph, features
+
+def load_yelp():
+    dataset = FraudYelpDataset()
+    
+    graph = dgl.to_homogeneous(dataset[0]).to_networkx().to_undirected()
+    features = dataset[0].ndata['feature'].numpy()
+
+    return graph, features
