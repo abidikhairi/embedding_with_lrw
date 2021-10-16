24e59f039eabe156303d82c59832c4d9bc1712e9
flursky <kh41r1.abidi@gmail.com>
2021-10-16 18:04:34 +0100

    code refactoring

--- generate_walks.py
+++ generate_walks.py
@@ -1,27 +1,40 @@
-import dgl
+import argparse
 import numpy as np
-from scipy.spatial.distance import hamming, cosine, euclidean
+from tqdm import tqdm
 
-from walker import LazyRandomWalk, RandomWalk
-from utils import load_citeseer, load_cora, load_yelp, load_arxiv
+from walker import LazyRandomWalk
+from utils import load_data, get_similarity_func
 
 
-def main():
-    graph, features, _ = load_arxiv()
+def main(args):
+    dataset_name = args.dataset
+    similarity_func = get_similarity_func(args.similarity)
+    num_walks = args.num_walks
+    walk_length = args.walk_length
+    
+    
+    graph, features, _ = load_data(dataset_name)
 
-    for node, _ in graph.nodes(data=True):
+    for node, _ in tqdm(graph.nodes(data=True), desc='Init Node Features'):
         if hasattr(features[node], 'numpy'):
             graph.nodes[node]['node_attr'] = features[node].numpy()
         else:
             graph.nodes[node]['node_attr'] = features[node]
-
-    walks = LazyRandomWalk(graph, similarity=[cosine, euclidean]).simulate_walks()
+    
+    walks = LazyRandomWalk(graph, num_walks=num_walks, walk_length=walk_length, similarity=similarity_func).simulate_walks()
 
     np_walks = np.array(walks)
 
-    np.save('temp/arxiv-lrw-walks', np_walks)
+    np.save('temp/{}-lrw-walks'.format(dataset_name), np_walks)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True, help='dataset to use: arxiv, cora, yelp, citeseer')
+    parser.add_argument('--num_walks', default=5, type=int, required=False, help='Number of walks starting from each node')
+    parser.add_argument('--walk_length', default=80, type=int, required=False, help='Walk Length for each node')
+    parser.add_argument('--similarity', type=str, required=True, help='Similarity function to use e.g cosine, hamming, euclidean')
+    args = parser.parse_args()
+
+    main(args)
 
     
--- utils.py
+++ utils.py
@@ -1,5 +1,27 @@
+import dgl
 from ogb.nodeproppred import DglNodePropPredDataset
 from dgl.data import CoraGraphDataset, CiteseerGraphDataset, FraudYelpDataset
+
+def get_similarity_func(name):
+    import scipy.spatial.distance as distance
+    if name == 'cosine':
+        return distance.cosine
+    elif name == 'euclidean':
+        return distance.euclidean
+    elif name == 'hamming':
+        return distance.hamming
+    elif name == 'taxi':
+        return distance.minkowski
+
+def load_data(name):
+    if name == 'arxiv':
+        return load_arxiv()
+    elif name == 'cora':
+        return load_cora()
+    elif name == 'citeseer':
+        return load_citeseer()
+    elif name == 'yelp':
+        return load_yelp()
 
 def load_arxiv():
     dataset = DglNodePropPredDataset(name='ogbn-arxiv', root='temp')
@@ -15,23 +37,26 @@ def load_arxiv():
 def load_cora():
     dataset = CoraGraphDataset(verbose=False)
 
-    features = dataset[0].ndata['feat']
+    features = dataset[0].ndata['feat'].numpy()
     graph = dataset[0].to_networkx().to_undirected()
+    labels = graph.ndata['label'].numpy()
 
-    return graph, features
+    return graph, features, labels
 
 def load_citeseer():
     dataset = CiteseerGraphDataset(verbose=False)
 
-    features = dataset[0].ndata['feat']
+    features = dataset[0].ndata['feat'].numpy()
     graph = dataset[0].to_networkx().to_undirected()
+    labels = graph.ndata['label'].numpy()
 
-    return graph, features
+    return graph, features, labels
 
 def load_yelp():
     dataset = FraudYelpDataset()
     
     graph = dgl.to_homogeneous(dataset[0]).to_networkx().to_undirected()
     features = dataset[0].ndata['feature'].numpy()
+    labels = graph.ndata['label'].numpy()
 
-    return graph, features
+    return graph, features, labels
