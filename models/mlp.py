8ca21ffab00cc14b2f338037c7086bf0d5864179
flursky <kh41r1.abidi@gmail.com>
2021-10-18 15:40:44 +0100

    change mlp to handle scikit-learn models

--- models/mlp.py
+++ models/mlp.py
@@ -1,8 +1,5 @@
 import torch
-import torchmetrics.functional as thmetrics
 from torch import nn
-from torch.utils.data import TensorDataset, DataLoader, random_split
-
 
 class MLP(nn.Module):
     def __init__(self, feature_size, hidden_size, num_classes):
@@ -22,38 +19,31 @@ class MLP(nn.Module):
 
 class MLPTrainer():
 
-    def __init__(self, features, labels, num_classes, feature_size, hidden_size, batch_size = 128, train_ratio = 0.8, device = 'cpu'):
+    def __init__(self, num_classes, feature_size, hidden_size, device = 'cpu'):
 
-        self.features = features
-        self.labels = labels
         self.mlp = MLP(feature_size=feature_size, hidden_size=hidden_size, num_classes=num_classes)
         self.criterion = nn.CrossEntropyLoss()
         self.device = torch.device(device)
-        self.dataset = TensorDataset(features, labels)
-        self.batch_size = batch_size
-        self.train_ratio = train_ratio
-    
-    def loader(self):
-        num_train = int(len(self.dataset) * self.train_ratio)
-        num_test = len(self.dataset) - num_train
-        trainset, testset = random_split(self.dataset, [num_train, num_test])
-        return DataLoader(trainset, batch_size=self.batch_size, shuffle=True), DataLoader(testset, batch_size=self.batch_size, shuffle=True)
-
-    @torch.no_grad()
-    def evaluate(self, loader):
-        acc = []
-        for features, labels in loader:
-            features, labels = features.to(self.device), labels.to(self.device)
-            logits = self.mlp(features)
-            acc.append(thmetrics.accuracy(logits, labels))
         
-        return torch.tensor(acc).mean()
-
-    def train(self, epochs = 10, learning_rate = 0.01):
-        trainloader, testloader = self.loader()
+    @torch.no_grad()
+    def evaluate(self, x_test):
+        x_test = torch.tensor(x_test).unsqueeze(1).to(self.device)
+        
+        logits = self.mlp(x_test)
+        _, preds = torch.argmax(logits)
+        
+        return preds.numpy()
+    
+    def predict(self, x_test):
+        return self.evaluate(x_test)
+        
+    def train(self, epochs = 10, learning_rate = 0.001):
+        trainloader, _ = self.loader()
+        
+        self.mlp.to(self.device)
         optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
 
-        for epoch in range(epochs):
+        for _ in range(epochs):
             for features, labels in trainloader:
                 self.mlp.train()
                 optimizer.zero_grad()
@@ -64,7 +54,11 @@ class MLPTrainer():
                 loss = self.criterion(logits, labels)
                 loss.backward()
                 optimizer.step()
-                
-                accuracy = self.evaluate(testloader)
 
-                print('Epoch: {}, Train loss: {}, Accuracy: {:.4f} %'.format(epoch, loss, accuracy))
+    def fit(self, x_train, y_train):
+        x_train = torch.tensor(x_train).unsqueeze(1).to(self.device)
+        y_train = torch.tensor(y_train).unsqueeze(1).to(self.device)
+
+        self.train()
+
+        return self
