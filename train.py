15cd200c932d3f051ea8e18aeccd9e978350a8ad
flursky <kh41r1.abidi@gmail.com>
2021-10-19 13:51:03 +0100

    arxiv experiment 0.1151 %

--- train.py
+++ train.py
@@ -1,40 +1,14 @@
 import argparse
 import logging
 import numpy as np
-from node2vec import Node2Vec
-from gensim.models import Word2Vec
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
-from utils import load_arxiv, load_yelp, load_cora, load_citeseer
+
+from utils import load_arxiv, load_yelp, load_cora, load_citeseer, load_fifa
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-def run_skipgram(walk_path):
-
-    walks = np.load(walk_path).tolist()
-
-    skipgram = Word2Vec(sentences=walks, vector_size=128, negative=5, window=8, sg=1, workers=6, epochs=1)
-    
-    keys = list(map(int, skipgram.wv.index_to_key))
-    keys.sort()
-    
-    vectors = [skipgram.wv[key] for key in keys]
-
-    return np.array(vectors)
-
-def run_node2vec(graph):
-    node2vec = Node2Vec(graph, dimensions=128, walk_length=80, num_walks=5, p=1, q=1, workers=6)
-
-    word2vec = node2vec.fit(negative=5, window=8, epochs=1)
-
-    keys = list(map(int, word2vec.wv.index_to_key))
-    
-    keys.sort()
-
-    vectors = [word2vec.wv[key] for key in keys]
-    
-    return np.array(vectors)
 
 def run_classifier(embeddings, labels, train_ratio=0.8):
     """
@@ -46,9 +20,9 @@ def run_classifier(embeddings, labels, train_ratio=0.8):
     test_accuracies = []
     
 
-    classifier = LogisticRegression(multi_class='ovr', n_jobs=7)
+    classifier = LogisticRegression(multi_class='ovr', n_jobs=6)
             
-    for _ in range(1):
+    for _ in range(5):
         classifier = classifier.fit(x_train, y_train)
                 
         train_acc = accuracy_score(y_train, classifier.predict(x_train))
@@ -64,19 +38,11 @@ def run_classifier(embeddings, labels, train_ratio=0.8):
     print('\t\t\t: Test Accuracy: {:.4f} %'.format(np.mean(test_accuracies) * 100))
     print('-'*120)
 
-def train():
-    graph, _, _ = load_cora()
-
-    embeddings = run_skipgram(walk_path='temp/cora-lrw-walks.npy')
-    np.save('temp/embeddings/cora-lrw', embeddings)
-
-    embeddings = run_node2vec(graph)
-    np.save('temp/embeddings/cora-node2vec', embeddings)
 
 def experiment():
-    _, _, labels = load_cora()
-    lrw_emebdding = np.load('temp/embeddings/cora-lrw.npy')
-    node2vec_emebdding = np.load('temp/embeddings/cora-node2vec.npy')
+    _, _, labels = load_arxiv()
+    lrw_emebdding = np.load('temp/embeddings/arxiv-lrw.npy')
+    node2vec_emebdding = np.load('temp/embeddings/arxiv-node2vec.npy')
 
     print('----- LRW + SkipGram + Logistic Regression -----')
     run_classifier(lrw_emebdding, labels, train_ratio=0.8)
@@ -85,12 +51,4 @@ def experiment():
     run_classifier(node2vec_emebdding, labels, train_ratio=0.8)
 
 if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--method', default='train', help='Task to run, experiment or train', choices=['train', 'experiment'], required=True)
-    args = parser.parse_args()
-
-    if args.method == 'train':
-        train()
-    else:
-        experiment()
+    experiment()
